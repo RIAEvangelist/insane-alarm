0b79fbadb523daebb2916bf66926b14a5cb4e289
Brandon Nozaki Miller <brandon@diginow.it>
2015-08-21 08:42:16 -0700

    added build files.

---
+++ build.js
@@ -0,0 +1,22 @@
+var NwBuilder = require('nw-builder');
+var nw = new NwBuilder(
+    {
+        files: './src/**/**', // use the glob format
+        platforms: ['osx', 'win','linux'],
+        winIco: './src/app/resources/icons/-128.ico',
+        macZip:true
+    }
+);
+
+nw.on('log',  console.log);
+
+// Build returns a promise
+nw.build().then(
+    function () {
+        console.log('all done!');
+    }
+).catch(
+    function (error) {
+        console.error(error);
+    }
+);
